f108669efc2cc84f00cbff81d57c8ee019f0cd36
vikasgupta-820 <vikasboyka820@gmail.com>
2024-06-14 22:03:11 +0530

    Updates

--- src/helper-funtion.js
+++
@@ -1,9 +0,0 @@
-export function isAllNodeConnected(nodes, edges) {
-  const allNodesIds = nodes.map((node) => node.id);
-  const allSourceEdges = edges.map((edge) => edge.source);
-  const count = allNodesIds.filter(nodeId => !allSourceEdges.includes(nodeId)).length;
-  if (count >= 2) {
-    return false;
-  }
-  return true;
-}
